fcea232aaf5c10c6ddc7593c9876008ccf3c9be0
luochenhuan <zhenhaiyucelia@gmail.com>
2015-09-19 04:17:46 -0400

    add and test webRTC

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,23 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+(function() {
+    var params = {},
+        r = /([^&=]+)=?([^&]*)/g;
 
+    function d(s) {
+        return decodeURIComponent(s.replace(/\+/g, ' '));
+    }
+
+    var match, search = window.location.search;
+    while (match = r.exec(search.substring(1))) {
+        params[d(match[1])] = d(match[2]);
+
+        if(d(match[2]) === 'true' || d(match[2]) === 'false') {
+            params[d(match[1])] = d(match[2]) === 'true' ? true : false;
+        }
+    }
+
+    window.params = params;
+    console.log(params)
+})();
