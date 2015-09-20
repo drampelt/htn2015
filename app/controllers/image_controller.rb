ed437cb879a18d3c9bf5c94acad94c226a5f6767
Daniel <drampelt@gmail.com>
2015-09-20 03:00:52 +0000

    facial recognition stuff

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,14 +1,11 @@
-require 'indico'
-require 'base64'
-
 class ImageController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def send_image
-	image_analysis = (Indico.fer(params[:image]))
-	render json: image_analysis
+  	image_analysis = Indico.fer params[:image]
+  	render json: image_analysis
   end
   
 end
