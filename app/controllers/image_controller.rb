288739099a412774360eac50288f4e191a7b5eef
Daniel <drampelt@gmail.com>
2015-09-20 04:28:19 +0000

    better face detection

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -4,7 +4,11 @@ class ImageController < ActionController::Base
   protect_from_forgery with: :exception
 
   def send_image
-  	image_analysis = Indico.fer params[:image]
+    if params[:detect] == "true"
+      image_analysis = Indico.fer params[:image], {"detect" => true}
+    else
+  	  image_analysis = Indico.fer params[:image]
+  	end
   	render json: image_analysis
   end
   
