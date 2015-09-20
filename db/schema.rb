880647fc5f4964c397d0a2a11faa012510bba6de
Daniel <drampelt@gmail.com>
2015-09-20 06:46:53 +0000

    add notes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150920060243) do
+ActiveRecord::Schema.define(version: 20150920064639) do
 
   create_table "interviews", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20150920060243) do
   end
 
   add_index "interviews", ["user_id"], name: "index_interviews_on_user_id"
+
+  create_table "notes", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "interview_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "notes", ["interview_id"], name: "index_notes_on_interview_id"
 
   create_table "questions", force: :cascade do |t|
     t.text     "content"
